bdd771d08a1271dcc3b330487b4e4d668f60fd5e
Brage Nilsson <brage.son@gmail.com>
2015-04-12 22:14:22 +0200

    Update PolyhedronGeometry to r71

--- lib/extras/geometries/octahedron_geometry.dart
+++ lib/extras/geometries/octahedron_geometry.dart
@@ -1,14 +1,20 @@
+/*
+ * @author timothypratley / https://github.com/timothypratley
+ *
+ * based on r71
+ */
+
 part of three;
 
 class OctahedronGeometry extends PolyhedronGeometry {
-  factory OctahedronGeometry(double radius, num detail) {
-    var vertices = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]];
+  factory OctahedronGeometry(double radius, int detail) {
+    var vertices = [1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1];
 
-    var faces = [[0, 2, 4], [0, 4, 3], [0, 3, 5], [0, 5, 2], [1, 2, 5], [1, 5, 3], [1, 3, 4], [1, 4, 2]];
+    var faces = [0, 2, 4, 0, 4, 3, 0, 3, 5, 0, 5, 2, 1, 2, 5, 1, 5, 3, 1, 3, 4, 1, 4, 2];
 
     return new OctahedronGeometry._internal(vertices, faces, radius, detail);
   }
 
-  OctahedronGeometry._internal(List<List<num>> vertices, List<List<num>> faces, double radius, num details)
+  OctahedronGeometry._internal(List<int> vertices, List<int> faces, double radius, int details)
       : super(vertices, faces, radius, details);
 }
--- lib/extras/geometries/polyhedron_geometry.dart
+++ lib/extras/geometries/polyhedron_geometry.dart
@@ -1,29 +1,55 @@
+/*
+ * @author clockworkgeek / https://github.com/clockworkgeek
+ * @author timothypratley / https://github.com/timothypratley
+ * @author WestLangley / http://github.com/WestLangley
+ *
+ * based on r71
+ */
+
 part of three;
 
 class PolyhedronGeometry extends Geometry {
-  List _midpoints;
-
-  // nelsonsilva - We're using a PolyhedronGeometryVertex decorator to allow adding index and uv properties
-  List<Vector3> _p = [];
-
-  PolyhedronGeometry(List<List<num>> lvertices, List<List<num>> lfaces, [double radius = 1.0, num detail = 0])
+  PolyhedronGeometry(List<int> vertices, List<int> indices, [double radius = 1.0, int detail = 0])
       : super() {
-    _midpoints = [];
+    for (var i = 0; i < vertices.length; i += 3) {
+      _prepare(new Vector3(vertices[i].toDouble(), vertices[i + 1].toDouble(), vertices[i + 2].toDouble()));
+    }
 
-    lvertices.forEach((vertex) {
-      _prepare(new Vector3(vertex[0].toDouble(), vertex[1].toDouble(), vertex[2].toDouble()));
+    var faces = [];
+
+    for (var i = 0, j = 0; i < indices.length; i += 3, j++) {
+      var v1 = this.vertices[indices[i    ]];
+      var v2 = this.vertices[indices[i + 1]];
+      var v3 = this.vertices[indices[i + 2]];
+
+      faces.add(new Face3(v1._index, v2._index, v3._index, normal: [v1.clone(), v2.clone(), v3.clone()]));
+    }
+
+    faces.forEach((face) => _subdivide(face, detail));
+
+    // Handle case when face straddles the seam
+    faceVertexUvs[0].forEach((uvs) {
+      var x0 = uvs[0].x;
+      var x1 = uvs[1].x;
+      var x2 = uvs[2].x;
+
+      var max = Math.max(x0, Math.max(x1, x2));
+      var min = Math.min(x0, Math.min(x1, x2));
+
+      if (max > 0.9 && min < 0.1) { // 0.9 is somewhat arbitrary
+        if (x0 < 0.2) uvs[0].x += 1;
+        if (x1 < 0.2) uvs[1].x += 1;
+        if (x2 < 0.2) uvs[2].x += 1;
+      }
     });
 
-    lfaces.forEach((face) => _make(_p[face[0]], _p[face[1]], _p[face[2]], detail));
+    // Apply radius
+    this.vertices.forEach((vertex) => vertex.scale(radius));
 
-    // TODO No need to unwrap ? (now unwrapp and add the original Vector3 to the vertices)
-    _p.forEach((v) => this.vertices.add(v));
-
+    // Merge vertices
     mergeVertices();
 
-    // Apply radius
-
-    this.vertices.forEach((Vector3 vertex) => vertex.scale(radius));
+    computeFaceNormals();
 
     computeCentroids();
 
@@ -31,96 +57,94 @@ class PolyhedronGeometry extends Geometry {
   }
 
   // Project vector onto sphere's surface
-  Vector3 _prepare(Vector3 vertex) {
-    vertex.normalize();
-    _p.add(vertex);
-    vertex._index = _p.length - 1;
+  Vector3 _prepare(Vector3 vector) {
+    var vertex = vector.normalize().clone();
+    vertices.add(vertex);
+    vertex._index = vertices.length - 1;
 
     // Texture coords are equivalent to map coords, calculate angle and convert to fraction of a circle.
 
-    var u = _azimuth(vertex) / 2 / Math.PI + 0.5;
-    var v = _inclination(vertex) / Math.PI + 0.5;
-    vertex._uv = new Vector2(u, 1 - v);
+    var u = _azimuth(vector) / 2 / Math.PI + 0.5;
+    var v = _inclination(vector) / Math.PI + 0.5;
+    vertex._uv = new Vector2(u, 1.0 - v);
 
     return vertex;
   }
 
   // Approximate a curved face with recursively sub-divided triangles.
-  _make(Vector3 v1, Vector3 v2, Vector3 v3, num detail) {
-    if (detail < 1) {
-      var face = new Face3(v1._index, v2._index, v3._index, normal: [v1.clone(), v2.clone(), v3.clone()]);
-      face.centroid.add(v1).add(v2).add(v3).scale(1.0 / 3.0);
-      face.normal = face.centroid.clone().normalize();
-      this.faces.add(face);
+  void _make(Vector3 v1, Vector3 v2, Vector3 v3) {
+    var face = new Face3(v1._index, v2._index, v3._index, normal: [v1.clone(), v2.clone(), v3.clone()]);
+    faces.add(face);
 
-      var azi = _azimuth(face.centroid);
-      this.faceVertexUvs[0].add([_correctUV(v1._uv, v1, azi), _correctUV(v2._uv, v2, azi), _correctUV(v3._uv, v3, azi)]);
-    } else {
-      detail -= 1;
+    var centroid = (v1 + v2 + v3) * (1 / 3);
 
-      // split triangle into 4 smaller triangles
+    var azi = _azimuth(centroid);
 
-      _make(v1, _midpoint(v1, v2), _midpoint(v1, v3), detail); // top quadrant
-      _make(_midpoint(v1, v2), v2, _midpoint(v2, v3), detail); // left quadrant
-      _make(_midpoint(v1, v3), _midpoint(v2, v3), v3, detail); // right quadrant
-      _make(_midpoint(v1, v2), _midpoint(v2, v3), _midpoint(v1, v3), detail); // center quadrant
-    }
+    faceVertexUvs[0].add([_correctUV(v1._uv, v1, azi), _correctUV(v2._uv, v2, azi), _correctUV(v3._uv, v3, azi)]);
   }
 
-  _midpoint(Vector3 v1, Vector3 v2) {
-    // TODO - nelsonsilva - refactor this code
-    // arrays don't "automagically" grow in Dart!
-    if (_midpoints.length < v1._index + 1) {
-      _midpoints.length = v1._index + 1;
-      _midpoints[v1._index] = [];
-    }
-    if (_midpoints.length < v2._index + 1) {
-      _midpoints.length = v2._index + 1;
-      _midpoints[v2._index] = [];
-    }
+  // Analytically subdivide a face to the required detail level.
+  void _subdivide(Face3 face, int detail) {
+    var cols = Math.pow(2, detail);
+    var a = _prepare(vertices[face.a]);
+    var b = _prepare(vertices[face.b]);
+    var c = _prepare(vertices[face.c]);
+    var v = [];
 
-    // prepare _midpoints[ v1.index ][ v2.index ]
-    if (_midpoints[v1._index] == null) {
-      _midpoints[v1._index] = [];
-    }
+    // Construct all of the vertices for this subdivision.
 
-    if (_midpoints[v1._index].length < v2._index + 1) {
-      _midpoints[v1._index].length = v2._index + 1;
-    }
+    for (var i = 0 ; i <= cols; i ++) {
+      v.add([]);
 
-    // prepare _midpoints[ v2.index ][ v1.index ]
-    if (_midpoints[v2._index] == null) {
-      _midpoints[v2._index] = [];
-    }
+      var result = new Vector3.zero();
+      Vector3.mix(a, c, i / cols, result);
+      var aj = _prepare(result);
+      Vector3.mix(b, c, i / cols, result);
+      var bj = _prepare(result);
+      var rows = cols - i;
 
-    if (_midpoints[v2._index].length < v1._index + 1) {
-      _midpoints[v2._index].length = v1._index + 1;
-    }
-
-    var mid = _midpoints[v1._index][v2._index];
-
-    if (mid == null) {
-
-      // generate mean point and project to surface with prepare()
-      mid = _prepare(new Vector3.copy((v1 + v2).scale(0.5)));
-      _midpoints[v1._index][v2._index] = mid;
-      _midpoints[v2._index][v1._index] = mid;
+      for (var j = 0; j <= rows; j ++) {
+        if (j == 0 && i == cols) {
+          v[i].add(aj);
+        } else {
+          Vector3.mix(aj, bj, j / rows, result);
+          v[i].add(_prepare(result));
+        }
+      }
     }
 
-    return mid;
+    // Construct all of the faces.
+
+    for (var i = 0; i < cols ; i ++) {
+      for (var j = 0; j < 2 * (cols - i) - 1; j ++) {
+        var k = (j / 2).floor();
+
+        if (j % 2 == 0) {
+          _make(v[i][k + 1], v[i + 1][k], v[i][k]);
+        } else {
+          _make(v[i][k + 1], v[i + 1][k + 1], v[i + 1][k]);
+        }
+      }
+    }
   }
 
   /// Angle around the Y axis, counter-clockwise when looking from above.
   double _azimuth(Vector3 vector) => Math.atan2(vector.z, -vector.x);
 
   /// Angle above the XZ plane.
-  double _inclination(Vector3 vector) =>
-      Math.atan2(-vector.y, Math.sqrt((vector.x * vector.x) + (vector.z * vector.z)));
+  double _inclination(Vector3 vector) {
+    return Math.atan2(-vector.y, Math.sqrt(vector.x * vector.x + vector.z * vector.z));
+  }
 
   /// Texture fixing helper. Spheres have some odd behaviours.
   Vector2 _correctUV(Vector2 uv, Vector3 vector, double azimuth) {
-    if ((azimuth < 0) && (uv.x == 1)) uv = new Vector2(uv.x - 1, uv.y);
-    if ((vector.x == 0) && (vector.z == 0)) uv = new Vector2(azimuth / 2 / Math.PI + 0.5, uv.y);
-    return uv;
+    if (azimuth < 0 && uv.x == 1.0) {
+      uv = new Vector2(uv.x - 1.0, uv.y);
+    }
+    if (vector.x == 0.0 && vector.z == 0.0) {
+      uv = new Vector2(azimuth / 2 / Math.PI + 0.5, uv.y);
+    }
+
+    return uv.clone();
   }
 }
