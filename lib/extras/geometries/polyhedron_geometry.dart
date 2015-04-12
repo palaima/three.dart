e8c8fbe08e69030cc963b63352e471a170e859c6
Brage Nilsson <brage.son@gmail.com>
2015-04-12 13:05:09 +0200

    Vector3: add _uv and _index field

--- lib/extras/geometries/polyhedron_geometry.dart
+++ lib/extras/geometries/polyhedron_geometry.dart
@@ -4,14 +4,14 @@ class PolyhedronGeometry extends Geometry {
   List _midpoints;
 
   // nelsonsilva - We're using a PolyhedronGeometryVertex decorator to allow adding index and uv properties
-  List<PolyhedronGeometryVertex> _p = [];
+  List<Vector3> _p = [];
 
   PolyhedronGeometry(List<List<num>> lvertices, List<List<num>> lfaces, [double radius = 1.0, num detail = 0])
       : super() {
     _midpoints = [];
 
     lvertices.forEach((vertex) {
-      _prepare(new PolyhedronGeometryVertex(vertex[0], vertex[1], vertex[2]));
+      _prepare(new Vector3(vertex[0].toDouble(), vertex[1].toDouble(), vertex[2].toDouble()));
     });
 
     lfaces.forEach((face) => _make(_p[face[0]], _p[face[1]], _p[face[2]], detail));
@@ -31,30 +31,30 @@ class PolyhedronGeometry extends Geometry {
   }
 
   // Project vector onto sphere's surface
-  PolyhedronGeometryVertex _prepare(PolyhedronGeometryVertex vertex) {
+  Vector3 _prepare(Vector3 vertex) {
     vertex.normalize();
     _p.add(vertex);
-    vertex.index = _p.length - 1;
+    vertex._index = _p.length - 1;
 
     // Texture coords are equivalent to map coords, calculate angle and convert to fraction of a circle.
 
     var u = _azimuth(vertex) / 2 / Math.PI + 0.5;
     var v = _inclination(vertex) / Math.PI + 0.5;
-    vertex.uv = new Vector2(u, 1 - v);
+    vertex._uv = new Vector2(u, 1 - v);
 
     return vertex;
   }
 
   // Approximate a curved face with recursively sub-divided triangles.
-  _make(PolyhedronGeometryVertex v1, PolyhedronGeometryVertex v2, PolyhedronGeometryVertex v3, num detail) {
+  _make(Vector3 v1, Vector3 v2, Vector3 v3, num detail) {
     if (detail < 1) {
-      var face = new Face3(v1.index, v2.index, v3.index, [v1.clone(), v2.clone(), v3.clone()]);
+      var face = new Face3(v1._index, v2._index, v3._index, [v1.clone(), v2.clone(), v3.clone()]);
       face.centroid.add(v1).add(v2).add(v3).scale(1.0 / 3.0);
       face.normal = face.centroid.clone().normalize();
       this.faces.add(face);
 
       var azi = _azimuth(face.centroid);
-      this.faceVertexUvs[0].add([_correctUV(v1.uv, v1, azi), _correctUV(v2.uv, v2, azi), _correctUV(v3.uv, v3, azi)]);
+      this.faceVertexUvs[0].add([_correctUV(v1._uv, v1, azi), _correctUV(v2._uv, v2, azi), _correctUV(v3._uv, v3, azi)]);
     } else {
       detail -= 1;
 
@@ -67,44 +67,44 @@ class PolyhedronGeometry extends Geometry {
     }
   }
 
-  _midpoint(v1, v2) {
+  _midpoint(Vector3 v1, Vector3 v2) {
     // TODO - nelsonsilva - refactor this code
     // arrays don't "automagically" grow in Dart!
-    if (_midpoints.length < v1.index + 1) {
-      _midpoints.length = v1.index + 1;
-      _midpoints[v1.index] = [];
+    if (_midpoints.length < v1._index + 1) {
+      _midpoints.length = v1._index + 1;
+      _midpoints[v1._index] = [];
     }
-    if (_midpoints.length < v2.index + 1) {
-      _midpoints.length = v2.index + 1;
-      _midpoints[v2.index] = [];
+    if (_midpoints.length < v2._index + 1) {
+      _midpoints.length = v2._index + 1;
+      _midpoints[v2._index] = [];
     }
 
     // prepare _midpoints[ v1.index ][ v2.index ]
-    if (_midpoints[v1.index] == null) {
-      _midpoints[v1.index] = [];
+    if (_midpoints[v1._index] == null) {
+      _midpoints[v1._index] = [];
     }
 
-    if (_midpoints[v1.index].length < v2.index + 1) {
-      _midpoints[v1.index].length = v2.index + 1;
+    if (_midpoints[v1._index].length < v2._index + 1) {
+      _midpoints[v1._index].length = v2._index + 1;
     }
 
     // prepare _midpoints[ v2.index ][ v1.index ]
-    if (_midpoints[v2.index] == null) {
-      _midpoints[v2.index] = [];
+    if (_midpoints[v2._index] == null) {
+      _midpoints[v2._index] = [];
     }
 
-    if (_midpoints[v2.index].length < v1.index + 1) {
-      _midpoints[v2.index].length = v1.index + 1;
+    if (_midpoints[v2._index].length < v1._index + 1) {
+      _midpoints[v2._index].length = v1._index + 1;
     }
 
-    var mid = _midpoints[v1.index][v2.index];
+    var mid = _midpoints[v1._index][v2._index];
 
     if (mid == null) {
 
       // generate mean point and project to surface with prepare()
-      mid = _prepare(new PolyhedronGeometryVertex.fromVector3((v1 + v2).scale(0.5)));
-      _midpoints[v1.index][v2.index] = mid;
-      _midpoints[v2.index][v1.index] = mid;
+      mid = _prepare(new Vector3.copy((v1 + v2).scale(0.5)));
+      _midpoints[v1._index][v2._index] = mid;
+      _midpoints[v2._index][v1._index] = mid;
     }
 
     return mid;
@@ -123,15 +123,4 @@ class PolyhedronGeometry extends Geometry {
     if ((vector.x == 0) && (vector.z == 0)) uv = new Vector2(azimuth / 2 / Math.PI + 0.5, uv.y);
     return uv;
   }
-}
-
-/**
- * [PolyhedronGeometryVertex] is a [Vector3] decorator to allow introducing [index] and [uv].
- * */
-class PolyhedronGeometryVertex extends Vector3 {
-  int index;
-  Vector2 uv;
-  PolyhedronGeometryVertex([num x = 0.0, num y = 0.0, num z = 0.0]) : super(x.toDouble(), y.toDouble(), z.toDouble());
-
-  PolyhedronGeometryVertex.fromVector3(Vector3 other) : super.copy(other);
 }
