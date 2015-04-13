2d9231227b0e658552601af2b6091919f0719ec2
Brage Nilsson <brage.son@gmail.com>
2015-04-13 18:27:11 +0200

    Bone: clean-up

--- lib/src/objects/bone.dart
+++ lib/src/objects/bone.dart
@@ -1,47 +1,42 @@
+/*
+ * @author mikael emtinger / http://gomo.se/
+ * @author alteredq / http://alteredqualia.com/
+ *
+ * based on r66
+ */
+
 part of three;
 
 /// A bone which is part of a SkinnedMesh.
 class Bone extends Object3D {
   /// The skin that contains this bone.
   SkinnedMesh skin;
-  /// The matrix of the bone.
-  Matrix4 skinMatrix;
 
-  Bone(this.skin)
-      : skinMatrix = new Matrix4.identity(),
-        super();
+  /// The matrix of the bone.
+  Matrix4 skinMatrix = new Matrix4.identity();
+
+  Bone(this.skin) : super();
 
   /// This updates the matrix of the bone and the matrices of its children.
-  update([Matrix4 parentSkinMatrix, bool forceUpdate = false]) {
-
+  void update([Matrix4 parentSkinMatrix, bool forceUpdate = false]) {
     // update local
     if (matrixAutoUpdate) {
-
-      // This should be <=> forceUpdate |= updateMatrix();
       if (forceUpdate) updateMatrix();
     }
 
     // update skin matrix
-
     if (forceUpdate || matrixWorldNeedsUpdate) {
-
       if (parentSkinMatrix != null) {
-
         skinMatrix = parentSkinMatrix * matrix;
-
       } else {
-
-        skinMatrix = matrix.clone();
-
+        skinMatrix.setFrom(matrix);
       }
 
       matrixWorldNeedsUpdate = false;
       forceUpdate = true;
-
     }
 
     // update children
-    children.forEach((c) => c.update(skinMatrix, forceUpdate));
-
+    children.forEach((child) => child.update(skinMatrix, forceUpdate));
   }
 }
