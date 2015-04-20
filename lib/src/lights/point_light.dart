eadd59cf12b17be626263ae5faf6140e400350b4
Brage Nilsson <brage.son@gmail.com>
2015-04-20 23:07:20 +0200

    Update PointLight

--- lib/src/lights/point_light.dart
+++ lib/src/lights/point_light.dart
@@ -1,26 +1,23 @@
-part of three;
-
-/**
- * @author mr.doob / http://mrdoob.com/
- *
- * Ported to Dart from JS by:
- * @author rob silverton / http://www.unwrong.com/
+/*
+ * @author mrdoob / http://mrdoob.com/
  */
+
+part of three;
 
 /// Affects objects using MeshLambertMaterial or MeshPhongMaterial.
 class PointLight extends Light {
-  Vector3 _position;
+  String type = 'PointLight';
+
   /// Light's intensity.
   double intensity;
   /// If non-zero, light will attenuate linearly from maximum intensity at light position down to zero at distance.
   double distance;
 
+  double decay;
+
   /// Creates a light at a specific position in the scene.
   ///
   /// The light shines in all directions (roughly similar to a light bulb.)
-  PointLight(num hex, {this.intensity: 1.0, this.distance: 0.0}) : super(hex) {
-    //THREE.Light.call( this, hex );
-
-    _position = new Vector3.zero();
+  PointLight(num color, {this.intensity: 1.0, this.distance: 0.0, this.decay: 1.0}) : super(color) {
   }
 }
