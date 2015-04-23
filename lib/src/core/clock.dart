493f1524cc5aa31fda467cdf9b9212ba58640688
Brage Nilsson <brage.son@gmail.com>
2015-04-23 17:07:58 +0200

    Clock: refactoring

--- lib/src/core/clock.dart
+++ lib/src/core/clock.dart
@@ -8,49 +8,52 @@ part of three;
 
 /// Class for keeping track of time.
 class Clock {
-  /// Whether the clock starts automatically on first update.
-  bool autoStart;
-
-  /// Returns the time when the clock was started.
-  double startTime = 0.0;
-
-  /// The time when [getDelta] was last called.
-  double oldTime = 0.0;
-
-  /// Elapsed seconds since [start] was called.
-  double elapsedTime = 0.0;
-
-  /// Whether the clock is running.
-  bool running = false;
+  bool _autoStart;
+  double _startTime = 0.0;
+  double _oldTime = 0.0;
+  double _elapsedTime = 0.0;
+  bool _running = false;
 
   /// Creates a new clock object. If [autoStart] is set, the clock starts
   /// automatically when [getDelta] is first called.
-  Clock([this.autoStart = true]);
+  Clock([this._autoStart = true]);
+
+  /// Whether the clock is running.
+  bool get running => _running;
+
+  /// Time when clock was started.
+  double get startTime => _startTime;
+
+  /// Elapsed seconds since [start] was called.
+  double get elapsedTime {
+    getDelta();
+    return _elapsedTime;
+  }
 
   /// Starts the clock.
   void start() {
-    startTime = window.performance.now();
-    oldTime = startTime;
-    running = true;
+    _startTime = window.performance.now();
+    _oldTime = _startTime;
+    _running = true;
   }
 
   /// Stops the clock.
   void stop() {
     getDelta();
-    running = false;
+    _running = false;
   }
 
   /// Returns seconds passed a since the last call to this method.
   double getDelta() {
     var diff;
 
-    if (autoStart && !running) start();
+    if (_autoStart && !_running) start();
 
-    if (running) {
+    if (_running) {
       var newTime = window.performance.now();
-      diff = 0.001 * (newTime - oldTime);
-      oldTime = newTime;
-      elapsedTime += diff;
+      diff = 0.001 * (newTime - _oldTime);
+      _oldTime = newTime;
+      _elapsedTime += diff;
     }
 
     return diff;
