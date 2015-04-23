e86808454278d1ce8baa43a7131077e17f6c1430
Brage Nilsson <brage.son@gmail.com>
2015-04-23 17:08:59 +0200

    Add PlaneBufferGeometry

---
+++ lib/extras/geometries/plane_buffer_geometry.dart
@@ -0,0 +1,84 @@
+/*
+ * @author mrdoob / http://mrdoob.com/
+ * based on a5cc2899aafab2461c52e4b63498fb284d0c167b
+ */
+
+part of three;
+
+class PlaneBufferGeometry extends BufferGeometry {
+  String type = 'PlaneBufferGeometry';
+
+  PlaneBufferGeometry(double width, double height, [int widthSegments = 1, int heightSegments = 1]) {
+    var width_half = width / 2;
+    var height_half = height / 2;
+
+    var gridX = widthSegments;
+    var gridY = heightSegments;
+
+    var gridX1 = gridX + 1;
+    var gridY1 = gridY + 1;
+
+    var segment_width = width / gridX;
+    var segment_height = height / gridY;
+
+    var vertices = new Float32List(gridX1 * gridY1 * 3);
+    var normals = new Float32List(gridX1 * gridY1 * 3);
+    var uvs = new Float32List(gridX1 * gridY1 * 2);
+
+    var offset = 0;
+    var offset2 = 0;
+
+    for (var iy = 0; iy < gridY1; iy ++) {
+      var y = iy * segment_height - height_half;
+
+      for (var ix = 0; ix < gridX1; ix ++) {
+        var x = ix * segment_width - width_half;
+
+        vertices[offset    ] = x;
+        vertices[offset + 1] = - y;
+
+        normals[offset + 2] = 1.0;
+
+        uvs[offset2    ] = ix / gridX;
+        uvs[offset2 + 1] = 1.0 - (iy / gridY);
+
+        offset += 3;
+        offset2 += 2;
+      }
+    }
+
+    offset = 0;
+
+    var indices;
+
+    if ((vertices.length / 3) > 65535) {
+      indices = new Uint32List(gridX * gridY * 6);
+    } else {
+      indices = new Uint16List(gridX * gridY * 6);
+    }
+
+    for (var iy = 0; iy < gridY; iy ++) {
+      for (var ix = 0; ix < gridX; ix ++) {
+        var a = ix + gridX1 * iy;
+        var b = ix + gridX1 * (iy + 1);
+        var c = (ix + 1) + gridX1 * (iy + 1);
+        var d = (ix + 1) + gridX1 * iy;
+
+        indices[offset    ] = a;
+        indices[offset + 1] = b;
+        indices[offset + 2] = d;
+
+        indices[offset + 3] = b;
+        indices[offset + 4] = c;
+        indices[offset + 5] = d;
+
+        offset += 6;
+      }
+    }
+
+    aIndex = new BufferAttribute(indices, 1);
+    aPosition = new BufferAttribute(vertices, 3);
+    aNormal = new BufferAttribute(normals, 3);
+    aUV = new BufferAttribute(uvs, 2);
+  }
+}
--- lib/extras/geometries/plane_geometry.dart
+++ lib/extras/geometries/plane_geometry.dart
@@ -5,63 +5,17 @@
  * Ported to Dart from JS by:
  * @author rob silverton / http://www.unwrong.com/
  *
- * based on r66
+ * based on a5cc2899aafab2461c52e4b63498fb284d0c167b
  */
 
 part of three;
 
-/**
- * A class for generating plane geometries.
- *
- *     var geometry = new PlaneGeometry(5.0, 20.0);
- *     var material = new MeshBasicMaterial(color: 0xffff00, side: DOUBLE_SIDE);
- *     var plane = new Mesh(geometry, material);
- *     scene.add(plane);
- */
 class PlaneGeometry extends Geometry {
+  String type = 'PlaneGeometry';
+
   /// Creates a new plane geometry.
   PlaneGeometry(double width, double height, [int widthSegments = 1, int heightSegments = 1]) : super() {
-    var width_half = width / 2;
-    var height_half = height / 2;
-
-    var gridX = widthSegments;
-    var gridZ = heightSegments;
-
-    var gridX1 = gridX + 1;
-    var gridZ1 = gridZ + 1;
-
-    var segment_width = width / gridX;
-    var segment_height = height / gridZ;
-
-    var normal = new Vector3(0.0, -1.0, 0.0);
-
-    for (var iz = 0; iz < gridZ1; iz++) {
-      for (var ix = 0; ix < gridX1; ix++) {
-        var x = ix * segment_width - width_half;
-        var y = iz * segment_height - height_half;
-
-        vertices.add(new Vector3(x, -y, 0.0));
-      }
-    }
-
-    for (var iz = 0; iz < gridZ; iz++) {
-      for (var ix = 0; ix < gridX; ix++) {
-        var a = ix + gridX1 * iz;
-        var b = ix + gridX1 * (iz + 1);
-        var c = (ix + 1) + gridX1 * (iz + 1);
-        var d = (ix + 1) + gridX1 * iz;
-
-        var uva = new Vector2(ix / gridX, 1 - iz / gridZ);
-        var uvb = new Vector2(ix / gridX, 1 - (iz + 1) / gridZ);
-        var uvc = new Vector2((ix + 1) / gridX, 1 - (iz + 1) / gridZ);
-        var uvd = new Vector2((ix + 1) / gridX, 1 - iz / gridZ);
-
-        faces.add(new Face3(a, b, d, normal: [normal.clone(), normal.clone(), normal.clone()]));
-        faceVertexUvs[0].add([uva, uvb, uvd]);
-
-        faces.add(new Face3(b, c, d, normal: [normal.clone(), normal.clone(), normal.clone()]));
-        faceVertexUvs[0].add([uvb.clone(), uvc, uvd.clone()]);
-      }
-    }
+    log('PlaneGeometry: Consider using PlaneBufferGeometry for lower memory footprint.');
+    fromBufferGeometry(new PlaneBufferGeometry(width, height, widthSegments, heightSegments));
   }
 }
