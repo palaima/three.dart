b338f2634f9835629bab98dc8aedf134ba23699a
Brage Nilsson <brage.son@gmail.com>
2015-04-26 00:38:10 +0200

    Add InterleavedBufferAttribute

--- lib/src/core/interleaved_buffer_attribute.dart
+++ lib/src/core/interleaved_buffer_attribute.dart
@@ -1,5 +1,9 @@
 part of three;
 
 class InterleavedBufferAttribute {
+  InterleavedBuffer data;
+  int itemSize;
+  int offset;
 
+  InterleavedBufferAttribute(this.data, this.itemSize, this.offset);
 }
