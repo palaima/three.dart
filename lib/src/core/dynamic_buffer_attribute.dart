034dc443421dca98c6e1ac5a1036be1828002dfb
Brage Nilsson <brage.son@gmail.com>
2015-04-26 00:37:52 +0200

    Add DynamicBufferAttribute

--- lib/src/core/dynamic_buffer_attribute.dart
+++ lib/src/core/dynamic_buffer_attribute.dart
@@ -1,5 +1,16 @@
+/*
+ * @author benaadams / https://twitter.com/ben_a_adams
+ */
+
 part of three;
 
-class DynamicBufferAttribute {
+class DynamicBufferAttribute extends BufferAttribute {
+  Map updateRange = {'offset': 0, 'count': -1};
 
+  DynamicBufferAttribute(TypedData array, int itemSize)
+      : super(array, itemSize);
+
+  clone() {
+    throw new UnimplementedError();
+  }
 }
