52d5fe603e69909d5810476c2e86a33c9b03ba2f
Brage Nilsson <brage.son@gmail.com>
2015-04-25 23:10:25 +0200

    Update EffectComposer

--- lib/extras/postprocessing/effect_composer.dart
+++ lib/extras/postprocessing/effect_composer.dart
@@ -1,98 +1,118 @@
-part of three_postprocessing;
-
-/**
+/*
  * @author alteredq / http://alteredqualia.com/
  *
- * Ported to Dart from JS by:
- * @author Christopher Grabowski / https://github.com/cgrabowski
+ * based on r71
  */
 
+part of three.postprocessing;
+
 class EffectComposer {
-
   WebGLRenderer renderer;
-  WebGLRenderTarget readTarget;
+
+  WebGLRenderTarget renderTarget1;
+  WebGLRenderTarget renderTarget2;
+
+  WebGLRenderTarget writeBuffer;
+  WebGLRenderTarget readBuffer;
+
   WebGLRenderTarget writeTarget;
+  WebGLRenderTarget readTarget;
+
+  List<Pass> passes = [];
+
   ShaderPass copyPass;
-  List<Pass> passes = new List<Pass>();
-  bool maskActive = false;
 
-  EffectComposer(this.renderer, [WebGLRenderTarget target = null]) {
-    this.renderer.autoClear = false;
+  EffectComposer(this.renderer, [WebGLRenderTarget renderTarget]) {
+    if (renderTarget == null) {
+      var pixelRatio = renderer.getPixelRatio();
 
-    if (target == null) {
-      target = new WebGLRenderTarget(
-          window.innerWidth,
-          window.innerHeight,
-          minFilter: LinearFilter,
-          magFilter: LinearFilter,
-          format: RGBFormat,
-          stencilBuffer: false);
+      var width  = (renderer.context.canvas.width / pixelRatio).floor();
+      var height = (renderer.context.canvas.height / pixelRatio).floor();
+
+      if (width == 0) width = 1;
+      if (height == 0) height = 1;
+
+      renderTarget = new WebGLRenderTarget(width, height, minFilter: LinearFilter,
+          magFilter: LinearFilter, format: RGBFormat, stencilBuffer: false);
     }
-    writeTarget = target;
-    readTarget = target.clone();
-    copyPass = new ShaderPass(new ShaderProgram.fromThreeish(CopyShader));
+
+    renderTarget1 = renderTarget;
+    renderTarget2 = renderTarget.clone();
+
+    writeBuffer = renderTarget1;
+    readBuffer = renderTarget2;
+
+    copyPass = new ShaderPass(Shaders.copy);
   }
 
   void swapBuffers() {
-    var tmp = readTarget;
-    readTarget = writeTarget;
-    writeTarget = tmp;
+    var tmp = readBuffer;
+    readBuffer = writeBuffer;
+    writeBuffer = tmp;
   }
 
-  void addPass(pass) => passes.add(pass);
+  void addPass(Pass pass) {
+    passes.add(pass);
+  }
 
-  void insertPass(pass, index) => passes.insert(index, pass);
+  void insertPass(Pass pass, int index) {
+    passes.insert(index, pass);
+  }
 
-  void render(double delta) {
+  void render([double delta]) {
+    writeBuffer = renderTarget1;
+    readBuffer = renderTarget2;
 
-    maskActive = false;
+    var maskActive = false;
 
-    passes.forEach((Pass pass) {
-      if (!pass.enabled) return;
-
-      pass.render(renderer, writeTarget, readTarget, delta, maskActive);
+    passes.where((p) => p.enabled).forEach((pass) {
+      pass.render(renderer, writeBuffer, readBuffer, delta, maskActive);
 
       if (pass.needsSwap) {
-
         if (maskActive) {
           var context = renderer.context;
 
           context.stencilFunc(context.NOTEQUAL, 1, 0xffffffff);
-          copyPass.render(renderer, writeTarget, readTarget, delta, maskActive);
+
+          copyPass.render(renderer, writeBuffer, readBuffer, delta);
+
           context.stencilFunc(context.EQUAL, 1, 0xffffffff);
         }
 
         swapBuffers();
       }
 
-      /*
       if (pass is MaskPass) {
         maskActive = true;
       } else if (pass is ClearMaskPass) {
         maskActive = false;
       }
-       */
     });
   }
 
-  void reset([WebGLRenderTarget target = null, int width, int height]) {
+  void reset(WebGLRenderTarget renderTarget) {
+    if (renderTarget == null) {
+      renderTarget = renderTarget1.clone();
 
-    if (width == null) {
-      width = window.innerWidth;
+      var pixelRatio = renderer.getPixelRatio();
+
+      renderTarget.width  = (renderer.context.canvas.width  / pixelRatio).floor();
+      renderTarget.height = (renderer.context.canvas.height / pixelRatio).floor();
     }
-    if (height == null) {
-      height = window.innerHeight;
-    }
-    if (target == null) {
-      target = new WebGLRenderTarget(
-          width,
-          height,
-          minFilter: LinearFilter,
-          magFilter: LinearFilter,
-          format: RGBFormat,
-          stencilBuffer: false);
-    }
-    writeTarget = target;
-    readTarget = target.clone();
+
+    renderTarget1 = renderTarget;
+    renderTarget2 = renderTarget.clone();
+
+    writeBuffer = renderTarget1;
+    readBuffer = renderTarget2;
+  }
+
+  void setSize(width, height) {
+    var renderTarget = renderTarget1.clone();
+
+    renderTarget.width = width;
+    renderTarget.height = height;
+
+    reset(renderTarget);
   }
 }
