6e1e397aa1f7b0efcb7d2c7d5349b6b289ec45ff
Brage Nilsson <brage.son@gmail.com>
2015-04-25 23:17:33 +0200

    Update postprocessing.dart and move it up a directory + remove pass.dart

---
+++ lib/extras/postprocessing.dart
@@ -0,0 +1,30 @@
+library three.postprocessing;
+
+import 'dart:html';
+import 'dart:math' as Math;
+import 'dart:typed_data';
+import 'dart:web_gl' as gl;
+import 'package:three/three.dart';
+import 'package:three/extras/shaders.dart' as Shaders;
+import 'package:three/extras/uniforms_utils.dart' as UniformsUtils;
+import 'package:three/extras/three_math.dart' as ThreeMath;
+
+part 'postprocessing/adaptive_tone_mapping_pass.dart';
+part 'postprocessing/bloom_pass.dart';
+part 'postprocessing/bokeh_pass.dart';
+part 'postprocessing/dot_screen_pass.dart';
+part 'postprocessing/effect_composer.dart';
+part 'postprocessing/film_pass.dart';
+part 'postprocessing/glitch_pass.dart';
+part 'postprocessing/mask_pass.dart';
+part 'postprocessing/render_pass.dart';
+part 'postprocessing/save_pass.dart';
+part 'postprocessing/shader_pass.dart';
+part 'postprocessing/texture_pass.dart';
+
+abstract class Pass {
+  bool enabled;
+  bool needsSwap;
+  void render(renderer, writeBuffer, readBuffer, delta, maskActive);
+}
+
