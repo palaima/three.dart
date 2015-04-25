787dea36bb7c8e3e59b6e189c769da97281c3392
Brage Nilsson <brage.son@gmail.com>
2015-04-26 00:38:25 +0200

    Add InstancedInterleavedBuffer

--- lib/src/core/instanced_interleaved_buffer.dart
+++ lib/src/core/instanced_interleaved_buffer.dart
@@ -1,5 +1,13 @@
+/*
+ * @author benaadams / https://twitter.com/ben_a_adams
+ */
+
+
 part of three;
 
-class InstancedInterleavedBuffer {
+class InstancedInterleavedBuffer extends InterleavedBuffer {
+  int meshPerAttribute;
 
+  InstancedInterleavedBuffer(TypedData array, stride, {bool dynamic: false, this.meshPerAttribute: 1})
+      : super(array, stride, dynamic: dynamic);
 }
