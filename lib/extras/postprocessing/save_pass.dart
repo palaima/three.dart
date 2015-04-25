b7316d5384650cc783847728a42a1fe13bca3b97
Brage Nilsson <brage.son@gmail.com>
2015-04-25 23:46:51 +0200

    Add SavePass

---
+++ lib/extras/postprocessing/save_pass.dart
@@ -0,0 +1,56 @@
+/*
+ * @author alteredq / http://alteredqualia.com/
+ *
+ * based on r71
+ */
+
+part of three.postprocessing;
+
+class SavePass implements Pass {
+  String textureID = "tDiffuse";
+
+  Map uniforms;
+
+  ShaderMaterial material;
+
+  WebGLRenderTarget renderTarget;
+
+  bool enabled = true;
+  bool needsSwap = false;
+  bool clear = false;
+
+  OrthographicCamera camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
+  Scene scene  = new Scene();
+
+  Mesh quad;
+
+  SavePass(this.renderTarget) {
+    var shader = Shaders.copy;
+
+    uniforms = UniformsUtils.clone(shader['uniforms']);
+
+    material = new ShaderMaterial(
+        uniforms: this.uniforms,
+        vertexShader: shader['vertexShader'],
+        fragmentShader: shader['fragmentShader']);
+
+    if (renderTarget == null) {
+      renderTarget = new WebGLRenderTarget(window.innerWidth, window.innerHeight,
+          minFilter: LinearFilter, magFilter: LinearFilter, format: RGBFormat, stencilBuffer: false);
+    }
+
+    quad = new Mesh(new PlaneBufferGeometry(2.0, 2.0), null);
+    scene.add(quad);
+  }
+
+  void render(WebGLRenderer renderer, WebGLRenderTarget writeBuffer, WebGLRenderTarget readBuffer,
+              double delta, bool maskActive) {
+    if (uniforms[textureID]) {
+      uniforms[textureID].value = readBuffer;
+    }
+
+    quad.material = material;
+
+    renderer.render(scene, camera, renderTarget: renderTarget, forceClear: clear);
+  }
+}
