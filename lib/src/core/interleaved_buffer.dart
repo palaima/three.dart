489f96196fa17e4371e24a41da44e7c91fdc5767
Brage Nilsson <brage.son@gmail.com>
2015-04-26 00:37:38 +0200

    Add InterleavedBuffer

--- lib/src/core/interleaved_buffer.dart
+++ lib/src/core/interleaved_buffer.dart
@@ -1,5 +1,20 @@
+/*
+ * @author benaadams / https://twitter.com/ben_a_adams
+ */
+
 part of three;
 
 class InterleavedBuffer {
+  List array;
+  int stride;
 
+  bool needsUpdate = false;
+
+  bool dynamic;
+
+  Map updateRange = {'offset': 0, 'count': -1};
+
+  InterleavedBuffer(TypedData array, this.stride, {this.dynamic: false}) {
+    this.array = array as List;
+  }
 }
