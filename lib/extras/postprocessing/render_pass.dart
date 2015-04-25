5a353fcdbe43b6cca1d34405e2aab9dc5ecfcfc1
Brage Nilsson <brage.son@gmail.com>
2015-04-25 23:50:05 +0200

    Update RenderPass

--- lib/extras/postprocessing/render_pass.dart
+++ lib/extras/postprocessing/render_pass.dart
@@ -1,37 +1,41 @@
-part of three_postprocessing;
-
-/**
+/*
  * @author alteredq / http://alteredqualia.com/
  *
- * Ported to Dart from JS by:
- * @author Christopher Grabowski / https://github.com/cgrabowski
+ * based on r71
  */
 
-class RenderPass extends Pass {
+part of three.postprocessing;
+
+class RenderPass implements Pass {
   Scene scene;
   Camera camera;
+
   Material overrideMaterial;
+
   Color clearColor;
   double clearAlpha;
+
   Color oldClearColor = new Color.white();
   double oldClearAlpha = 1.0;
+
   bool enabled = true;
   bool clear = true;
-  bool needsSwap = true;
+  bool needsSwap = false;
 
-  RenderPass(this.scene, this.camera, [this.overrideMaterial = null, this.clearColor = null, this.clearAlpha = 1.0]);
+  RenderPass(this.scene, this.camera, {this.overrideMaterial, this.clearColor, this.clearAlpha: 1.0});
 
-  void render(WebGLRenderer renderer, WebGLRenderTarget writeTarget, WebGLRenderTarget readTarget, double delta,
-      bool maskActive) {
-
+  void render(WebGLRenderer renderer, WebGLRenderTarget writeBuffer, WebGLRenderTarget readBuffer,
+              double delta, bool maskActive) {
     scene.overrideMaterial = overrideMaterial;
+
     if (clearColor != null) {
       oldClearColor.setFrom(renderer.getClearColor());
       oldClearAlpha = renderer.getClearAlpha();
+
       renderer.setClearColor(clearColor, clearAlpha);
     }
 
-    renderer.renderToTarget(scene, camera, writeTarget, clear);
+    renderer.render(scene, camera, renderTarget: readBuffer, forceClear: clear);
 
     if (clearColor != null) {
       renderer.setClearColor(oldClearColor, oldClearAlpha);
