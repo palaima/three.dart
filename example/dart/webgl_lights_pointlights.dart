135f5d9d3eb6dec5a0b881e361e80facd3c328ac
Brage Nilsson <brage.son@gmail.com>
2015-05-05 21:07:12 +0200

    Add webgl_lights_pointlights example

---
+++ example/dart/webgl_lights_pointlights.dart
@@ -0,0 +1,105 @@
+import 'dart:html';
+import 'dart:math' as math;
+import 'package:three/three.dart';
+
+PerspectiveCamera camera;
+Scene scene;
+WebGLRenderer renderer;
+
+PointLight light1, light2, light3, light4;
+
+Mesh object;
+
+BinaryLoader loader;
+
+Clock clock = new Clock();
+
+void init() {
+  camera = new PerspectiveCamera(50.0, window.innerWidth / window.innerHeight, 1.0, 1000.0)
+    ..position.z = 100.0;
+
+  scene = new Scene();
+
+  loader = new BinaryLoader(showStatus: true);
+  document.body.append(loader.statusDomElement);
+
+  var callback = (geometry) {
+    object = new Mesh(geometry, new MeshPhongMaterial(color: 0x555555, specular: 0xffffff, shininess: 50.0));
+    object.scale.splat(0.80);
+    scene.add(object);
+
+    loader.statusDomElement.style.display = 'none';
+  };
+
+  loader.load('obj/walt/WaltHead_bin.js').then(callback);
+
+  scene.add(new AmbientLight(0x000000));
+
+  var sphere = new SphereGeometry(0.5, 16, 8);
+
+  light1 = new PointLight(0xff0040, intensity: 2.0, distance: 50.0);
+  light1.add(new Mesh(sphere, new MeshBasicMaterial(color: 0xff0040)));
+  scene.add(light1);
+
+  light2 = new PointLight(0x0040ff, intensity: 2.0, distance: 50.0);
+  light2.add(new Mesh(sphere, new MeshBasicMaterial(color: 0x0040ff)));
+  scene.add(light2);
+
+  light3 = new PointLight(0x80ff80, intensity: 2.0, distance: 50.0);
+  light3.add(new Mesh(sphere, new MeshBasicMaterial(color: 0x80ff80)));
+  scene.add(light3);
+
+  light4 = new PointLight(0xffaa00, intensity: 2.0, distance: 50.0);
+  light4.add(new Mesh(sphere, new MeshBasicMaterial(color: 0xffaa00)));
+  scene.add(light4);
+
+  renderer = new WebGLRenderer()
+    ..setPixelRatio(window.devicePixelRatio)
+    ..setSize(window.innerWidth, window.innerHeight);
+  document.body.append(renderer.domElement);
+
+  //
+
+  window.onResize.listen(onWindowResize);
+}
+
+void onWindowResize(_) {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+void render() {
+  var time = new DateTime.now().millisecondsSinceEpoch * 0.0005;
+  var delta = clock.getDelta();
+
+  if (object != null) object.rotation.y -= 0.5 * delta;
+
+  light1.position.x = math.sin(time * 0.7) * 30;
+  light1.position.y = math.cos(time * 0.5) * 40;
+  light1.position.z = math.cos(time * 0.3) * 30;
+
+  light2.position.x = math.cos(time * 0.3) * 30;
+  light2.position.y = math.sin(time * 0.5) * 40;
+  light2.position.z = math.sin(time * 0.7) * 30;
+
+  light3.position.x = math.sin(time * 0.7) * 30;
+  light3.position.y = math.cos(time * 0.3) * 40;
+  light3.position.z = math.sin(time * 0.5) * 30;
+
+  light4.position.x = math.sin(time * 0.3) * 30;
+  light4.position.y = math.cos(time * 0.7) * 40;
+  light4.position.z = math.sin(time * 0.5) * 30;
+
+  renderer.render(scene, camera);
+}
+
+main() async {
+  init();
+
+  while (true) {
+    await window.animationFrame;
+    render();
+  }
+}
