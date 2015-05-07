58fcd152cb6aba2f9c38a1cdef1e45ba230f3dd0
Brage Nilsson <brage.son@gmail.com>
2015-05-07 23:40:33 +0200

    Fix webgl_custom_attributes

--- example/dart/webgl_custom_attributes.dart
+++ example/dart/webgl_custom_attributes.dart
@@ -1,73 +1,104 @@
 import 'dart:html';
-import 'dart:math' as Math;
+import 'dart:typed_data';
+import 'dart:math' as math;
 import 'package:three/three.dart';
-import 'package:three/extras/image_utils.dart' as ImageUtils;
+import 'package:three/extras/image_utils.dart' as image_utils;
+
+final vertexShader = '''
+uniform float amplitude;
+
+attribute float displacement;
+
+varying vec3 vNormal;
+varying vec2 vUv;
+
+void main() {
+  vNormal = normal;
+  vUv = ( 0.5 + amplitude ) * uv + vec2( amplitude );
+
+  vec3 newPosition = position + amplitude * normal * vec3( displacement );
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+}
+''';
+
+final fragmentShader = '''
+varying vec3 vNormal;
+varying vec2 vUv;
+
+uniform vec3 color;
+uniform sampler2D texture;
+
+void main() {
+  vec3 light = vec3( 0.5, 0.2, 1.0 );
+  light = normalize( light );
+
+  float dProd = dot( vNormal, light ) * 0.5 + 0.5;
+
+  vec4 tcolor = texture2D( texture, vUv );
+  vec4 gray = vec4( vec3( tcolor.r * 0.3 + tcolor.g * 0.59 + tcolor.b * 0.11 ), 1.0 );
+
+  gl_FragColor = gray * vec4( vec3( dProd ) * vec3( color ), 1.0 );
+}
+''';
 
 PerspectiveCamera camera;
 Scene scene;
-Renderer renderer;
+WebGLRenderer renderer;
 
 Map<String, Uniform> uniforms;
-Uniform<double> amplitude;
-Uniform<Color> color;
-Uniform<Texture> texture;
-
 Map<String, Attribute> attributes;
-Attribute<double> displacement;
 
 Mesh sphere;
 
-List<double> noise;
+List<double> noise = [];
+Float32List displacement;
+DynamicBufferAttribute displacementAttribute;
 
-var rnd = new Math.Random();
-
-void main() {
-  init();
-  animate(0);
-}
+Function random = new math.Random().nextDouble;
 
 void init() {
-  camera = new PerspectiveCamera(30.0, window.innerWidth / window.innerHeight, 1.0, 10000.0)..position.z = 300.0;
+  camera = new PerspectiveCamera(30.0, window.innerWidth / window.innerHeight, 1.0, 10000.0)
+    ..position.z = 300.0;
 
   scene = new Scene();
 
-  displacement = new Attribute.float();
-  attributes = {
-    "displacement": displacement
-  };
-
-  amplitude = new Uniform.float(1.0);
-  color = new Uniform.color(0xff2200);
-  texture = new Uniform.texture(ImageUtils.loadTexture("textures/water.jpg"));
+  attributes = {'displacement': 0};
 
   uniforms = {
-    "amplitude": amplitude,
-    "color": color,
-    "texture": texture
+    'amplitude': new Uniform.float(1.0),
+    'color': new Uniform.color(0xff2200),
+    'texture': new Uniform.texture(image_utils.loadTexture('textures/water.jpg'))
   };
 
-  texture.value.wrapS = texture.value.wrapT = RepeatWrapping;
+  uniforms['texture'].value.wrapS = uniforms['texture'].value.wrapT = RepeatWrapping;
 
   var shaderMaterial = new ShaderMaterial(
-      uniforms: uniforms,
-      attributes: attributes,
-      vertexShader: document.querySelector('#vertexshader').text,
-      fragmentShader: document.querySelector('#fragmentshader').text);
+      uniforms: uniforms, attributes: attributes, vertexShader: vertexShader, fragmentShader: fragmentShader);
 
   var radius = 50.0,
       segments = 128,
       rings = 64;
 
-  var geometry = new SphereGeometry(radius, segments, rings)..isDynamic = true;
+  var geometry = new SphereBufferGeometry(radius, segments, rings);
+
+  var positions = geometry.attributes['position'];
+  var vertexCount = positions.length;
+
+  displacement = new Float32List(vertexCount);
+  noise = new Float32List(vertexCount);
+
+  for (var v = 0; v < displacement.length; v++) {
+    displacement[v] = 0.0;
+    noise[v] = random() * 5;
+  }
+
+  displacementAttribute = new DynamicBufferAttribute(displacement, 1);
+
+  geometry.addAttribute('displacement', displacementAttribute);
 
   sphere = new Mesh(geometry, shaderMaterial);
 
   scene.add(sphere);
-
-  var vertices = (sphere.geometry as Geometry).vertices;
-
-  noise = new List.generate(vertices.length, (_) => rnd.nextDouble() * 5);
-  displacement.value.addAll(new List.filled(vertices.length, 0));
 
   renderer = new WebGLRenderer()
     ..setClearColor(0x050505)
@@ -78,41 +109,40 @@ void init() {
   window.onResize.listen(onWindowResize);
 }
 
-void onWindowResize(event) {
+void onWindowResize(_) {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-
-void animate(num time) {
-  window.requestAnimationFrame(animate);
-  render();
-}
-
 void render() {
-
   var time = new DateTime.now().millisecondsSinceEpoch * 0.01;
 
   sphere.rotation.y = sphere.rotation.z = 0.01 * time;
 
-  amplitude.value = 2.5 * Math.sin(sphere.rotation.y * 0.125);
-  color.value.offsetHSL(0.0005, 0.0, 0.0);
+  uniforms['amplitude'].value = 2.5 * math.sin(sphere.rotation.y * 0.125);
+  uniforms['color'].value.offsetHSL(0.0005, 0.0, 0.0);
 
-  for (var i = 0; i < displacement.value.length; i++) {
+  for (var i = 0; i < displacement.length; i++) {
+    displacement[i] = math.sin(0.1 * i + time);
 
-    displacement.value[i] = Math.sin(0.1 * i + time);
+    noise[i] += 0.5 * (0.5 - random());
+    noise[i] = noise[i].clamp(-5.0, 5.0);
 
-    noise[i] += 0.5 * (0.5 - rnd.nextDouble());
-    noise[i] = clamp(noise[i], -5, 5);
-
-    displacement.value[i] += noise[i];
-
+    displacement[i] += noise[i];
   }
 
-  displacement.needsUpdate = true;
+  displacementAttribute.needsUpdate = true;
 
   renderer.render(scene, camera);
+}
 
+main() async {
+  init();
+
+  while (true) {
+    await window.animationFrame;
+    render();
+  }
 }
