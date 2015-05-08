3d371942b9b205b8356e6531c596abafb2b8a4c3
Brage Nilsson <brage.son@gmail.com>
2015-05-08 15:01:59 +0200

    Add VertexNormalsHelper

--- lib/extras/helpers.dart
+++ lib/extras/helpers.dart
@@ -10,3 +10,4 @@ part 'helpers/bounding_box_helper.dart';
 part 'helpers/box_helper.dart';
 part 'helpers/camera_helper.dart';
 part 'helpers/point_light_helper.dart';
+part 'helpers/vertex_normals_helper.dart';
---
+++ lib/extras/helpers/vertex_normals_helper.dart
@@ -0,0 +1,68 @@
+part of three.extras.helpers;
+
+class VertexNormalsHelper extends LineSegments {
+  Object3D object;
+
+  double size;
+
+  Matrix3 normalMatrix;
+
+  VertexNormalsHelper(this.object, {this.size: 1.0, hex: 0xff0000, linewidth: 1.0})
+      : super(new Geometry(), new LineBasicMaterial(color: hex, linewidth: linewidth)) {
+    var faces = (object as GeometryObject).geometry.faces;
+
+    for (var i = 0; i < faces.length; i++) {
+      var face = faces[i];
+
+      for (var j = 0; j < face.vertexNormals.length; j++) {
+        geometry.vertices.addAll([new Vector3.zero(), new Vector3.zero()]);
+      }
+    }
+
+    matrixAutoUpdate = false;
+    normalMatrix = new Matrix3.identity();
+    update();
+  }
+
+  void update() {
+    object.updateMatrixWorld(force: true);
+
+    normalMatrix.copyNormalMatrix(this.object.matrixWorld);
+
+    var vertices = geometry.vertices;
+
+    var objectGeo = (object as GeometryObject).geometry;
+    var verts = objectGeo.vertices;
+    var faces = objectGeo.faces;
+
+    var worldMatrix = object.matrixWorld;
+
+    var idx = 0;
+
+    for (var i = 0; i < faces.length; i++) {
+      var face = faces[i];
+
+      for (var j = 0; j < face.vertexNormals.length; j++) {
+        var vertexId = face.indices[j];
+        var vertex = verts[vertexId];
+
+        var normal = face.vertexNormals[j];
+
+        vertices[idx].setFrom(vertex).applyMatrix4(worldMatrix);
+
+        _v..setFrom(normal)..applyMatrix3(normalMatrix)..normalize()..scale(size);
+
+        _v.add(vertices[idx]);
+        idx = idx + 1;
+
+        vertices[idx].setFrom(_v);
+        idx = idx + 1;
+      }
+    }
+
+    geometry.verticesNeedUpdate = true;
+  }
+
+
+  static final Vector3 _v = new Vector3.zero();
+}
