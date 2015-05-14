c3a8fde6850f83d844aab61ffab93541e6ba7f5e
Brage Nilsson <brage.son@gmail.com>
2015-05-14 19:09:57 +0200

    three.renderers: import Gyroscope

--- lib/src/renderers.dart
+++ lib/src/renderers.dart
@@ -6,9 +6,10 @@ import 'dart:typed_data';
 import 'dart:web_gl' as gl;
 import 'dart:math' as math;
 
-import 'package:three/extras/uniforms_utils.dart' as uniforms_utils;
+import '../extras/uniforms_utils.dart' as uniforms_utils;
 
-import 'package:three/extras/helpers.dart' show CameraHelper;
+import '../extras/helpers.dart' show CameraHelper;
+import '../extras/core.dart' show Gyroscope;
 
 import 'logging.dart';
 import 'constants.dart';
