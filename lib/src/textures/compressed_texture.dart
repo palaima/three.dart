e6e5826360c8a8f6a7b0f98ba8aab7fc1ee6045b
Brage Nilsson <brage.son@gmail.com>
2015-05-14 20:11:28 +0200

    Update CompressedTexture

--- lib/src/textures/compressed_texture.dart
+++ lib/src/textures/compressed_texture.dart
@@ -1,12 +1,21 @@
 part of three.textures;
 
 class CompressedTexture extends Texture {
-  var mipmaps;
-  var images;
-  CompressedTexture({this.mipmaps, width, height, format, type, mapping, wrapS, wrapT, magFilter, minFilter})
-      : super(null, mapping, wrapS, wrapT, magFilter, minFilter, format, type),
-        images = {
-        "width": width,
-        "height": height
-      };
+  List<Map> mipmaps;
+
+  bool flipY = false;
+
+  bool generateMipmaps = false;
+
+  CompressedTexture({this.mipmaps, width, height, format, type, mapping, wrapS, wrapT, magFilter, minFilter, anisotropy})
+      : super(null, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy) {
+    image = new CompressedImage(width: width, height: height);
+  }
+}
+
+class CompressedImage {
+  int width, height;
+  int format;
+  List<Map> mipmaps;
+  CompressedImage({this.width, this.height, this.format, this.mipmaps});
 }
