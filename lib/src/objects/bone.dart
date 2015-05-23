04520e2438fa79857111a7d4206e445956e10c8e
Brage Nilsson <brage.son@gmail.com>
2015-05-24 00:56:54 +0200

    Update Bone

--- lib/src/objects/bone.dart
+++ lib/src/objects/bone.dart
@@ -1,42 +1,21 @@
 /*
  * @author mikael emtinger / http://gomo.se/
  * @author alteredq / http://alteredqualia.com/
+ * @author ikerr / http://verold.com
  *
- * based on r66
+ * based on r71
  */
 
 part of three.objects;
 
 /// A bone which is part of a SkinnedMesh.
 class Bone extends Object3D {
+  String type = 'Bone';
+
   /// The skin that contains this bone.
   SkinnedMesh skin;
 
-  /// The matrix of the bone.
-  Matrix4 skinMatrix = new Matrix4.identity();
+  Map animationCache;
 
-  Bone(this.skin) : super();
-
-  /// This updates the matrix of the bone and the matrices of its children.
-  void update([Matrix4 parentSkinMatrix, bool forceUpdate = false]) {
-    // update local
-    if (matrixAutoUpdate) {
-      if (forceUpdate) updateMatrix();
-    }
-
-    // update skin matrix
-    if (forceUpdate || matrixWorldNeedsUpdate) {
-      if (parentSkinMatrix != null) {
-        skinMatrix = parentSkinMatrix * matrix;
-      } else {
-        skinMatrix.setFrom(matrix);
-      }
-
-      matrixWorldNeedsUpdate = false;
-      forceUpdate = true;
-    }
-
-    // update children
-    children.forEach((child) => child.update(skinMatrix, forceUpdate));
-  }
+  Bone(this.skin);
 }
