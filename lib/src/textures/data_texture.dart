6d14ab35eb41f75a368d730d1d6c7e1625c7c369
Brage Nilsson <brage.son@gmail.com>
2015-05-23 19:36:51 +0200

    DataTexture: clean-up

--- lib/src/textures/data_texture.dart
+++ lib/src/textures/data_texture.dart
@@ -1,16 +1,32 @@
+/*
+ * @author alteredq / http://alteredqualia.com/
+ */
+
 part of three.textures;
 
 class DataTexture extends Texture {
-  DataTexture._internal(image, data, width, height, format, [type, mapping, wrapS, wrapT, magFilter, minFilter])
+  DataTexture._internal(DataImage image, int width, int height, int format,
+      int type, int mapping, int wrapS, int wrapT, int magFilter, int minFilter)
       : super(image, mapping, wrapS, wrapT, magFilter, minFilter, format, type);
 
-  factory DataTexture(data, width, height, format, {type, mapping, wrapS, wrapT, magFilter, minFilter}) {
-    return new DataTexture._internal(new DataImage(data, width, height), data, width, height, format, type, mapping, wrapS, wrapT, magFilter, minFilter);
+  factory DataTexture(TypedData data, int width, int height,
+      {int format: RGBAFormat, int type: UnsignedByteType,
+      int mapping: Texture.defaultMapping, int wrapS: ClampToEdgeWrapping,
+      int wrapT: ClampToEdgeWrapping, int magFilter: LinearFilter,
+      int minFilter: LinearMipMapLinearFilter}) {
+    return new DataTexture._internal(new DataImage(data, width, height), width,
+        height, format, type, mapping, wrapS, wrapT, magFilter, minFilter);
+  }
+
+  DataTexture clone([DataTexture texture]) {
+    texture = new DataTexture(null, null, null);
+    super.clone(texture);
+    return texture;
   }
 }
 
 class DataImage {
-  Float32List data;
+  TypedData data;
   int width, height;
   DataImage(this.data, this.width, this.height);
 }
