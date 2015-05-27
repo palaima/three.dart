0535653d7039adc845cda3b2be7939d4faf78112
Brage Nilsson <brage.son@gmail.com>
2015-05-27 13:46:05 +0200

    Update DirectGeometry

--- lib/src/core/direct_geometry.dart
+++ lib/src/core/direct_geometry.dart
@@ -12,11 +12,27 @@ class DirectGeometry implements IGeometry {
   String name = '';
   String type = 'DynamicGeometry';
 
+  List indices = [];
   List<Vector3> vertices = [];
   List<Color> colors = [];
   List<Vector3> normals = [];
   List<Vector2> uvs = [];
-  List<Face3> faces = [];
+  List uvs2 = [];
+
+  /// List of [MorphTarget].
+  List morphTargets = [];
+
+  /// List of [MorphColor].
+  List morphColors = [];
+
+  /// List of [MorphNormal]
+  List morphNormals = [];
+
+  /// List of skinning weights, matching number and order of vertices.
+  List<Vector4> skinWeights = [];
+
+  /// List of skinning indices, matching number and order of vertices.
+  List<Vector4> skinIndices = [];
 
   Aabb3 boundingBox;
   Sphere boundingSphere;
@@ -28,25 +44,10 @@ class DirectGeometry implements IGeometry {
   bool colorsNeedUpdate = false;
   bool uvsNeedUpdate = false;
 
-  /// List of [MorphTarget].
-  List<MorphTarget> morphTargets;
-
-  /// List of [MorphColor].
-  List<MorphColor> morphColors;
-
-  /// List of [MorphNormal]
-  List<MorphNormal> morphNormals;
-
-  /// List of skinning weights, matching number and order of vertices.
-  List<Vector4> skinWeights;
-
-  /// List of skinning indices, matching number and order of vertices.
-  List<Vector4> skinIndices;
-
   DirectGeometry();
 
-  DirectGeometry.fromGeometry(Geometry geometry) {
-    setFromGeometry(geometry);
+  DirectGeometry.fromGeometry(Geometry geometry, Material material) {
+    setFromGeometry(geometry, material);
   }
 
   /// Computes bounding box of the geometry, updating Geometry.boundingBox.
@@ -71,61 +72,154 @@ class DirectGeometry implements IGeometry {
   }
 
   void computeFaceNormals() {
-    warn('DynamicGeometry: computeFaceNormals() is not a method of this type of geometry.');
+    warn(
+        'DynamicGeometry: computeFaceNormals() is not a method of this type of geometry.');
   }
 
   void computeVertexNormals() {
-    warn('DynamicGeometry: computeVertexNormals() is not a method of this type of geometry.');
+    warn(
+        'DynamicGeometry: computeVertexNormals() is not a method of this type of geometry.');
   }
 
-  void setFromGeometry(Geometry geometry) {
-    this.vertices = geometry.vertices;
-    this.faces = geometry.faces;
-
+  void setFromGeometry(Geometry geometry, [Material material]) {
     var faces = geometry.faces;
-    var faceVertexUvs = geometry.faceVertexUvs[0];
+    var vertices = geometry.vertices;
+    var faceVertexUvs = geometry.faceVertexUvs;
+    var materialVertexColors =
+        material != null ? material.vertexColors : NoColors;
 
-    normals.length = colors.length = uvs.length = geometry._maxFaceIndex;
+    var hasFaceVertexUv = faceVertexUvs.length > 0 && faceVertexUvs[0].length > 0;
+    var hasFaceVertexUv2 = faceVertexUvs.length > 1 && faceVertexUvs[1].length > 0;
+
+    // morphs
+
+    var morphTargets = geometry.morphTargets;
+    var morphTargetsLength = morphTargets.length;
+
+    for (var i = 0; i < morphTargetsLength; i++) {
+      this.morphTargets.add([]);
+    }
+
+    var morphNormals = geometry.morphNormals;
+    var morphNormalsLength = morphNormals.length;
+
+    for (var i = 0; i < morphNormalsLength; i++) {
+      this.morphNormals.add([]);
+    }
+
+    var morphColors = geometry.morphColors;
+    var morphColorsLength = morphColors.length;
+
+    for (var i = 0; i < morphColorsLength; i++) {
+      this.morphColors.add([]);
+    }
+
+    // skins
+
+    var skinIndices = geometry.skinIndices;
+    var skinWeights = geometry.skinWeights;
+
+    var hasSkinIndices = skinIndices.length == vertices.length;
+    var hasSkinWeights = skinWeights.length == vertices.length;
+
+    //
 
     for (var i = 0; i < faces.length; i++) {
       var face = faces[i];
-      var indices = [face.a, face.b, face.c];
+
+      this.vertices
+          .addAll([vertices[face.a], vertices[face.b], vertices[face.c]]);
 
       var vertexNormals = face.vertexNormals;
-      var vertexColors = face.vertexColors;
-      var vertexUvs;
 
-      for (var j = 0; j < vertexNormals.length; j++) {
-        normals[indices[j]] = vertexNormals[j];
-      }
-
-      for (var j = 0; j < vertexColors.length; j++) {
-        colors[indices[j]] = vertexColors[j];
-      }
-
-      if (faceVertexUvs.length > i) {
-        vertexUvs = faceVertexUvs[i];
+      if (vertexNormals.length == 3) {
+        this.normals
+            .addAll([vertexNormals[0], vertexNormals[1], vertexNormals[2]]);
       } else {
-        warn('DynamicGeometry.fromGeometry(): Missing vertexUVs $i');
-        vertexUvs = [new Vector2.zero(), new Vector2.zero(), new Vector2.zero()];
+        var normal = face.normal;
+
+        this.normals.addAll([normal, normal, normal]);
       }
 
-      for (var j = 0; j < vertexUvs.length; j++) {
-        uvs[indices[j]] = vertexUvs[j];
+      var vertexColors = face.vertexColors;
+
+      if (materialVertexColors == VertexColors) {
+        this.colors.addAll([vertexColors[0], vertexColors[1], vertexColors[2]]);
+      } else if (materialVertexColors == FaceColors) {
+        var color = face.color;
+
+        this.colors.addAll([color, color, color]);
+      }
+
+      if (hasFaceVertexUv) {
+        var vertexUvs = faceVertexUvs[0][i];
+
+        if (vertexUvs != null) {
+          this.uvs.addAll([vertexUvs[0], vertexUvs[1], vertexUvs[2]]);
+        } else {
+          warn('BufferGeometry.fromGeometry(): Undefined vertexUv $i');
+
+          this.uvs.addAll(
+              [new Vector2.zero(), new Vector2.zero(), new Vector2.zero()]);
+        }
+      }
+
+      if (hasFaceVertexUv2) {
+        var vertexUvs = faceVertexUvs[1][i];
+
+        if (vertexUvs != null) {
+          this.uvs2.addAll([vertexUvs[0], vertexUvs[1], vertexUvs[2]]);
+        } else {
+          warn('BufferGeometry.fromGeometry(): Undefined vertexUv2 $i');
+
+          this.uvs2.addAll(
+              [new Vector2.zero(), new Vector2.zero(), new Vector2.zero()]);
+        }
+      }
+
+      // morphs
+
+      for (var j = 0; j < morphTargetsLength; j++) {
+        var morphTarget = morphTargets[j].vertices;
+
+        this.morphTargets[j].addAll(
+            [morphTarget[face.a], morphTarget[face.b], morphTarget[face.c]]);
+      }
+      /*
+      for ( var j = 0; j < morphNormalsLength; j ++ ) {
+
+        var morphNormal = morphNormals[ j ].vertexNormals[ i ];
+
+        this.morphNormals[ j ].push( morphNormal.a, morphNormal.b, morphNormal.c );
+
+      }
+
+      for ( var j = 0; j < morphColorsLength; j ++ ) {
+
+        var morphColor = morphColors[ j ].colors;
+
+        this.morphColors[ j ].push( morphColor[ face.a ], morphColor[ face.b ], morphColor[ face.c ] );
+
+      }
+      */
+
+      // skins
+
+      if (hasSkinIndices) {
+        this.skinIndices.addAll(
+            [skinIndices[face.a], skinIndices[face.b], skinIndices[face.c]]);
+      }
+
+      if (hasSkinWeights) {
+        this.skinWeights.addAll([
+            skinWeights[face.a], skinWeights[face.b], skinWeights[face.c]]);
       }
     }
 
-    // Empty unused lists.
-    if (colors.every((e) => e == null)) colors.length = 0;
-    if (normals.every((e) => e == null)) normals.length = 0;
-    if (uvs.every((e) => e == null)) uvs.length = 0;
-
-    if (geometry.morphTargets != null) morphTargets = geometry.morphTargets.toList();
-    if (geometry.morphColors != null) morphColors = geometry.morphColors.toList();
-    if (geometry.morphNormals != null) morphNormals = geometry.morphNormals.toList();
-
-    if (geometry.skinIndices != null) skinIndices = geometry.skinIndices.toList();
-    if (geometry.skinWeights != null) skinWeights = geometry.skinWeights.toList();
+    this.verticesNeedUpdate = geometry.verticesNeedUpdate;
+    this.normalsNeedUpdate = geometry.normalsNeedUpdate;
+    this.colorsNeedUpdate = geometry.colorsNeedUpdate;
+    this.uvsNeedUpdate = geometry.uvsNeedUpdate;
   }
 
   StreamController _onDisposeController = new StreamController.broadcast();
